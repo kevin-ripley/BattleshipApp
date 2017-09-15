9cd7d3d64f7cc049d75be92478ee8b1e9923c6c9
folkpark <parkerfolkman@gmail.com>
2017-09-14 22:40:11 -0600

    Added html for boards; worked on client.py

--- client.py
+++ client.py
@@ -1,6 +1,19 @@
 import http.client as client
+import sys
+
+#get command line arguments
+str(sys.argv)
+if len(sys.argv) == 5:
+    Address = sys.argv[1]
+    port = sys.argv[2]
+    X = sys.argv[3]
+    Y = sys.argv[4]
+
+print("Battleship client")
 
 conn = client.HTTPSConnection("www.python.org")
 conn.request("GET", "/")
 r1 = conn.getresponse()
 print(r1.status, r1.reason)
+
+
