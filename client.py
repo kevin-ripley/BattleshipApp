0324ef262ccd4aefb3de76bb89d39bbef20c79b6
Kevin Ripley <kevin.r.ripley.kr@gmail.com>
2017-09-16 13:44:33 -0600

    able to connect via Get method from client to server

--- client.py
+++ client.py
@@ -8,8 +8,10 @@ port = int(sys.argv[2])
 X = int(sys.argv[3])
 Y = int(sys.argv[4])
 
-conn = client.HTTPConnection("localhost", port)
-conn.request("GET", "/")
+conn = client.HTTPConnection(Address, port)
+conn.request("GET", "/board.txt")
+response = client.HTTPResponse
 r1 = conn.getresponse()
-print(r1.status, r1.reason)
+body = response.read(r1, 256)
+print(r1.status, r1.reason, body)
 
--- server.py
+++ server.py
@@ -1,15 +1,17 @@
 import http.server
 import socketserver
 import sys
+import ipaddress as ip
 
 #This code parses command line arguments
 #argument format: server.py <PORT#> <board.txt>
 str(sys.argv)
-PORT = sys.argv[1] #
+port = sys.argv[1] #
 board = sys.argv[2]
 
 
 Handler = http.server.SimpleHTTPRequestHandler
-with socketserver.TCPServer(("", int(PORT)), Handler) as httpd:
-    print("serving at port", PORT)
+with socketserver.TCPServer(("", int(port)), Handler) as httpd:
+    print("Serving HTTP at port ", port)
+
     httpd.serve_forever()
