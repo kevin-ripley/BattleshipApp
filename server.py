9aba2174363dead9fff1195369a337c77806117b
folkpark <parkerfolkman@gmail.com>
2017-09-16 16:07:35 -0600

    Server now handles GET requests through do_GET method

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import http.server
+from http.server import BaseHTTPRequestHandler, HTTPServer
 import socketserver
 import sys
 import ipaddress as ip
@@ -9,9 +10,18 @@ str(sys.argv)
 port = sys.argv[1] #
 board = sys.argv[2]
 
+class ServerHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+        returnMessage = b"Hello World"
+        self.wfile.write(returnMessage, "utf8")
 
-Handler = http.server.SimpleHTTPRequestHandler
-with socketserver.TCPServer(("", int(port)), Handler) as httpd:
-    print("Serving HTTP at port ", port)
-
+def runServer():
+    server_address = ('localhost', 5000)
+    httpd = HTTPServer(server_address, ServerHandler)
+    print('server running on localhost port 5000')
     httpd.serve_forever()
+
+runServer()
