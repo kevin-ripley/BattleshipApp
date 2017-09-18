0ebb4cb8bc54236fbc18179cde5a8dad46c5ebe8
Kevin Ripley <kevin.r.ripley.kr@gmail.com>
2017-09-18 12:02:39 -0600

    logic completed

--- server.py
+++ server.py
@@ -82,7 +82,12 @@ class PostHandler(server.BaseHTTPRequestHandler):
                 board_count['c'] = c;
                 with open(newfile, 'wb') as file:
                     pickle.dump(board_count, file)
-                print("Carrier Hit!")
+                if (c == 0):
+                    self.send_response(200, "hit=1&sunkC")
+                    print("Carrier Sunk!")
+                else:
+                    self.send_response(200, "hit=1")
+                    print("Carrier Hit!")
             if (board[x][y] == 'B'):
                 self.send_response(200, "hit=1")
                 b = board_count.get('b')
@@ -90,7 +95,12 @@ class PostHandler(server.BaseHTTPRequestHandler):
                 board_count['b'] = b;
                 with open(newfile, 'wb') as file:
                     pickle.dump(board_count, file)
-                print("Battleship Hit!")
+                if (b == 0):
+                    self.send_response(200, "hit=1&sunkB")
+                    print("Battleship Sunk!")
+                else:
+                    self.send_response(200, "hit=1")
+                    print("Submarine Hit!")
             if (board[x][y] == 'R'):
                 self.send_response(200, "hit=1")
                 r = board_count.get('r')
@@ -98,7 +108,13 @@ class PostHandler(server.BaseHTTPRequestHandler):
                 board_count['r'] = r;
                 with open(newfile, 'wb') as file:
                     pickle.dump(board_count, file)
-                print("Cruiser Hit!")
+                if (r == 0):
+                    self.send_response(200, "hit=1&sunkR")
+                    print("Cruiser Sunk!")
+                else:
+                    self.send_response(200, "hit=1")
+                    print("Cruiser Hit!")
+
             if (board[x][y] == 'S'):
                 s = board_count.get('s')
                 s -= 1
@@ -125,7 +141,7 @@ class PostHandler(server.BaseHTTPRequestHandler):
                     print("Destroyer Hit!")
             else:
                 self.send_response(200, "hit=0")
-                print("MISS")
+                print("MISS, Try Again")
 
             self.send_header('Content-Type',
                          'text/plain; charset=utf-8')
