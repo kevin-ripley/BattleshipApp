cda625b97b7d84cbeae21b532ee55820a66ded43
Kevin Ripley <kevin.r.ripley.kr@gmail.com>
2017-09-18 11:21:07 -0600

    logic done
    + Cannot Fire on Location more than once
    + Will Send proper messages for hit, sunk, miss
    + Will send 410: HTTP Gone, 404: HTTP Not Found,  and now just need to
    send HTTP Bad Request if message not formatted correctly

--- server.py
+++ server.py
@@ -20,20 +20,33 @@ newlist = l.split('\n')
 board = newlist
 
 board_count = {'b': 4, 'c': 5, 'd': 2, 'r': 3, 's': 3}
-
+location = {'00': 0, '01': 0, '02': 0, '03': 0, '04': 0, '05': 0, '06': 0, '07': 0, '08': 0, '09': 0, '10': 0,
+            '11': 0, '12': 0, '13': 0, '14': 0, '15': 0, '16': 0, '17': 0, '18': 0, '19': 0, '20': 0,
+            '21': 0, '22': 0, '23': 0, '24': 0, '25': 0, '26': 0, '27': 0, '28': 0, '29': 0, '30': 0,
+            '31': 0, '32': 0, '33': 0, '34': 0, '35': 0, '36': 0, '37': 0, '38': 0, '39': 0, '40': 0,
+            '41': 0, '42': 0, '43': 0, '44': 0, '45': 0, '46': 0, '47': 0, '48': 0, '49': 0, '50': 0,
+            '51': 0, '52': 0, '53': 0, '54': 0, '55': 0, '56': 0, '57': 0, '58': 0, '59': 0, '60': 0,
+            '61': 0, '62': 0, '63': 0, '64': 0, '65': 0, '66': 0, '67': 0, '68': 0, '69': 0, '70': 0,
+            '71': 0, '72': 0, '73': 0, '74': 0, '75': 0, '76': 0, '77': 0, '78': 0, '79': 0, '80': 0,
+            '81': 0, '82': 0, '83': 0, '84': 0, '85': 0, '86': 0, '87': 0, '88': 0, '89': 0, '90': 0,
+            '91': 0, '92': 0, '93': 0, '94': 0, '95': 0, '96': 0, '97': 0, '98': 0, '99': 0, '100': 0,}
 newfile = 'board_count.pk'
+locate = 'location.pk'
 
 with open(newfile, 'wb') as file:
   pickle.dump(board_count, file)
 
+with open(locate, 'wb') as file:
+  pickle.dump(location, file)
+
 class PostHandler(server.BaseHTTPRequestHandler):
     global b, c, d, r, s
+    global coords
     def do_POST(self):
 
         with open(newfile, 'rb') as file:
             board_count = pickle.load(file)
-        board_list = board_count.items()
-        print(board_list)
+
         form = cgi.FieldStorage(
             fp=self.rfile,
             headers=self.headers,
@@ -43,70 +56,82 @@ class PostHandler(server.BaseHTTPRequestHandler):
             }
         )
 
-        global coords
         coords = []
         for field in form.keys():
             field_item = form[field]
             coords.append(form[field].value)
 
+        with open(locate, 'rb') as file:
+            loc = pickle.load(file)
         x = int(coords[0])
         y = int(coords[1])
 
+        coord = ''.join(str(i) for i in coords)
+        update = loc.get(coord)
+
         if (x >= 10 or y >= 10):
             self.send_error(404, "HTTP Not Found")
-        if (board[x][y] == 'C'):
-            self.send_response(200, "hit=1")
-            c = board_count.get('c')
-            c -= 1
-            board_count['c'] = c;
-            with open(newfile, 'wb') as file:
-                pickle.dump(board_count, file)
-            print("hit=1", + c)
-        if (board[x][y] == 'B'):
-            self.send_response(200, "hit=1")
-            b = board_count.get('b')
-            b -= 1
-            board_count['b'] = b;
-            with open(newfile, 'wb') as file:
-                pickle.dump(board_count, file)
-            print("Hit! with ", b, "hits")
-        if (board[x][y] == 'R'):
-            self.send_response(200, "hit=1")
-            r = board_count.get('r')
-            r -= 1
-            board_count['r'] = r;
-            with open(newfile, 'wb') as file:
-                pickle.dump(board_count, file)
-            print("hit=1")
-        if (board[x][y] == 'S'):
-            self.send_response(200, "hit=1")
-            s = board_count.get('s')
-            s -= 1
-            board_count['s'] = s;
-            with open(newfile, 'wb') as file:
-                pickle.dump(board_count, file)
-            print("hit=1")
-        if (board[x][y] == 'D'):
-            d = board_count.get('d')
-            print(d)
-            d -= 1
-            board_count['d'] = d;
-            with open(newfile, 'wb') as file:
-                pickle.dump(board_count, file)
-            if(d==0):
-                self.send_response(200, "hit=1&sunk=D")
-                print("hit=1&sunk=D")
-            else:
+        if(coord and update == 0):
+            loc[coord] = 1
+            with open(locate, 'wb') as file:
+                pickle.dump(loc, file)
+            if (board[x][y] == 'C'):
                 self.send_response(200, "hit=1")
+                c = board_count.get('c')
+                c -= 1
+                board_count['c'] = c;
+                with open(newfile, 'wb') as file:
+                    pickle.dump(board_count, file)
+                print("hit=1", + c)
+            if (board[x][y] == 'B'):
+                self.send_response(200, "hit=1")
+                b = board_count.get('b')
+                b -= 1
+                board_count['b'] = b;
+                with open(newfile, 'wb') as file:
+                    pickle.dump(board_count, file)
+                print("Hit! with ", b, "hits")
+            if (board[x][y] == 'R'):
+                self.send_response(200, "hit=1")
+                r = board_count.get('r')
+                r -= 1
+                board_count['r'] = r;
+                with open(newfile, 'wb') as file:
+                    pickle.dump(board_count, file)
                 print("hit=1")
-        else:
-            self.send_response(200, "hit=0")
-            print("MISS")
+            if (board[x][y] == 'S'):
+                self.send_response(200, "hit=1")
+                s = board_count.get('s')
+                s -= 1
+                board_count['s'] = s;
+                with open(newfile, 'wb') as file:
+                    pickle.dump(board_count, file)
+                print("hit=1")
+            if (board[x][y] == 'D'):
+                d = board_count.get('d')
+                print(d)
+                d -= 1
+                board_count['d'] = d;
+                with open(newfile, 'wb') as file:
+                    pickle.dump(board_count, file)
+                if(d==0):
+                    self.send_response(200, "hit=1&sunk=D")
+                    print("hit=1&sunk=D")
+                else:
+                    self.send_response(200, "hit=1")
+                    print("hit=1")
+            else:
+                self.send_response(200, "hit=0")
+                print("MISS")
 
-        self.send_header('Content-Type',
+            self.send_header('Content-Type',
                          'text/plain; charset=utf-8')
-        self.end_headers()
-
+            self.end_headers()
+        else:
+            self.send_response(410, "Try a different coordinate")
+            self.send_header('Content-Type',
+                                     'text/plain; charset=utf-8')
+            self.end_headers()
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-Type',
